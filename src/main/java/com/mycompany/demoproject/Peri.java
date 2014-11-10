aed5a81897904117a68cd144d54ea2aff337d61f
Achille <Achille@BBDamour>
2014-11-10 04:57:21 +0100

    class Peri

---
+++ src/main/java/com/mycompany/demoproject/Peri.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.demoproject;
+
+/**
+ *
+ * @author Achille
+ */
+public class Peri {
+    double hight;
+    double width;
+    
+    public Peri(String hight, String width){
+        this.hight = Double.parseDouble(hight);
+        this.width = Double.parseDouble(width);
+    }
+    
+    public double rec_peri(){
+        return 2*(this.hight + this.width); 
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,7 +3,7 @@
     Created on : 10.11.2014, 04:08:10
     Author     : Achille
 --%>
-
+<%@page import="com.mycompany.demoproject.Peri" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -13,5 +13,11 @@
     </head>
     <body>
         <h1>Hello World!</h1>
+        <% 
+            Peri test = new Peri("5.01", "4.01");
+            double perimeter = test.rec_peri();
+        %>
+        <h2> the perimeter is: <% out.print(perimeter); %> </h2>
+                
     </body>
 </html>
